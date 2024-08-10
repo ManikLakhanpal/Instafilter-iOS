0b6275be062184f44c545bc9a4168635f47595de
Lakhanpal Manik <lakhanpalmanik@gmail.com>
2024-08-10 17:53:09 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Manik Lakhanpal on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
