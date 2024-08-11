67651ed2702ed84331fdc1e11da220c480da4044
Lakhanpal Manik <lakhanpalmanik@gmail.com>
2024-08-11 16:52:17 +0530

    Added some modifications from scratch via adding a slider and a image.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -11,16 +11,49 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var amount = 0.0
     
     var body: some View {
-        ContentUnavailableView {
-            Label("No snippets", systemImage: "swift")
-        } description: {
-            Text("No data available right now :)")
-        } actions: {
-            Button("Create a snippet") {}
-                .buttonStyle(.borderedProminent)
+        NavigationStack {
+            Form {
+                Section("Set filter amount") {
+                    Text("Amount of Filter: \(amount, format: .number.precision(.fractionLength(2)))")
+                    Slider(value: $amount, in: 0...1)
+                        .onChange(of: amount, loadImage)
+                }
+                
+                VStack{
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onAppear(perform: loadImage)
+            }
+            .navigationTitle("Filter")
+            
         }
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
