8c5ea11419bbc7d60ed75a84afef49ae9ad5f843
Lakhanpal Manik <lakhanpalmanik@gmail.com>
2024-08-12 12:50:37 +0530

    Added Photos Picker to pick photos.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,55 +5,27 @@
 //  Created by Manik Lakhanpal on 10/08/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var amount = 0.0
+    @State private var pickerItem: PhotosPickerItem? // Allows us to select photos from library
+    @State private var selectedImage: Image?
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Set filter amount") {
-                    Text("Amount of Filter: \(amount, format: .number.precision(.fractionLength(2)))")
-                    Slider(value: $amount, in: 0...1)
-                        .onChange(of: amount, loadImage)
-                }
-                
-                VStack{
-                    image?
-                        .resizable()
-                        .scaledToFit()
-                }
-                .onAppear(perform: loadImage)
-            }
-            .navigationTitle("Filter")
+        VStack {
+            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
             
+            selectedImage?
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            }
+        .onChange(of: pickerItem) {
+            Task {
+                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            }
         }
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        
-        currentFilter.inputImage = beginImage
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
     }
 }
 
